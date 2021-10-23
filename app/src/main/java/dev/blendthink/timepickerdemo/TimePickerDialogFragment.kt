870f9fbd9929be306947fd5e87c7d3a3a1d6a7d6
blendthink <info@blendthink.dev>
2021-10-23 16:49:46 +0900

    add TimePickerDialogFragment

---
+++ app/src/main/java/dev/blendthink/timepickerdemo/TimePickerDialogFragment.kt
@@ -0,0 +1,23 @@
+package dev.blendthink.timepickerdemo
+
+import android.app.Dialog
+import android.app.TimePickerDialog
+import android.os.Bundle
+import android.util.Log
+import android.widget.TimePicker
+import androidx.fragment.app.DialogFragment
+import java.util.*
+
+class TimePickerDialogFragment : DialogFragment(), TimePickerDialog.OnTimeSetListener {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val c = Calendar.getInstance()
+        val hour = c[Calendar.HOUR_OF_DAY]
+        val minute = c[Calendar.MINUTE]
+        return TimePickerDialog(requireContext(), this, hour, minute, true)
+    }
+
+    override fun onTimeSet(view: TimePicker, hourOfDay: Int, minute: Int) {
+        Log.d("onTimeSet", "hourOfDay: $hourOfDay, minute: $minute")
+    }
+}
